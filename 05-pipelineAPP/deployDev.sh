02a117149ef7097091f850bcd671f35e9c5ff1b3
Erico <erico.hortolan@gmail.com>
2021-11-25 14:41:19 +0000

    commit 12

---
+++ 05-pipelineAPP/deployDev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd 05-pipelineAPP/ansible
+
+#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/chaveprivada.pem
+
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarDev.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
